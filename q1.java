47dd80b917ebb4d959ce1d9e16fd99eb8b22339c
logan-james <loganjames448@gmail.com>
2024-06-26 15:31:20 -0700

    Question one complete.

---
+++ q1.java
@@ -0,0 +1,17 @@
+public class q1 {
+  public static void main(String[] args) {
+    /* Type your code here. */
+    String[] pattern = {
+        "H   H",
+        "H   H",
+        "HHHHH",
+        "H   H",
+        "H   H"
+    };
+
+    for (String line : pattern) {
+      System.out.println(line);
+    }
+    System.out.println();
+  }
+}
