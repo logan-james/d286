5ebbd31a86e15d15224baf01376c160c8ea2d599
logan-james <loganjames448@gmail.com>
2024-06-27 09:15:40 -0700

    Simplier version of question 1

---
+++ q1alt.java
@@ -0,0 +1,9 @@
+public class q1alt {
+  public static void main(String[] args) {
+    System.out.println("H   H");
+    System.out.println("H   H");
+    System.out.println("HHHHH");
+    System.out.println("H   H");
+    System.out.println("H   H");
+  }
+}
