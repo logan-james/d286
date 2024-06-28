8305eb2e9a2dcc0b6e7c2179a0c82ad633da90ff
logan-james <loganjames448@gmail.com>
2024-06-28 13:15:40 -0700

    Question 13 solution.

---
+++ q13.java
@@ -0,0 +1,19 @@
+public class q13 {
+
+  public class RubberDuck extends Duck {
+
+    // TODO: Define getBehavior() overridden method and return String "squeaks"
+    public String getBehavior() {
+      return "squeaks";
+    }
+  }
+
+  public class MallardDuck extends Duck {
+
+    // TODO: Define getBehavior() overridden method and return String "flies"
+    public String getBehavior() {
+      return "flies";
+    }
+  }
+
+}
